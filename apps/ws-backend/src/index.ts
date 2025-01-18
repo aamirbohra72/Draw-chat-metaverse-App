2841c02dec96a231ace93c98507ac4c1ff34c1db
Aamir Bohra <aamir.bohra@skillnetinc.com>
2025-01-18 19:19:03 +0530

    first part complete

---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,29 @@
+import {WebSocketServer} from 'ws';
+import jwt, { JwtPayload } from "jsonwebtoken";
+import {JWT_SECRET} from '@repo/backend-common/config';
+
+const wss = new WebSocketServer({ port: 8080});
+
+wss.on('connection', function connection(ws, request){
+    const url = request.url;         //ws://localhost:8080/ws?token=123
+    if(!url) {return;}
+    
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get('token') || ""; 
+    const decoded = jwt.verify(token, 'JWT_SECRET');
+    
+    // way to check if token is valid or not 
+    // if(typeof decoded === 'string'){
+    //     ws.close();
+    //     return
+    // }
+
+    if(!decoded || !(decoded as JwtPayload).userId){
+        ws.close();
+        return;
+    }
+
+  ws.on('message', function message(data){
+      ws.send('pong');
+});
+});
